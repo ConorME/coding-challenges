a7b46abb542afe953774f5e040624712346aaa08
Conor <conor.esterly@gmail.com>
2025-01-06 17:24:45 -0500

    Additional 2 pointer implementation for find kth last node.

--- Linked List/kth_last.py
+++ Linked List/kth_last.py
@@ -13,3 +13,21 @@ def kth_last(head: 'Node', k: int) -> 'Node':
 
     return current
 
+def kth_last_2ptr(head: 'Node', k: int) -> 'Node':
+    if not head or k <= 0:
+        return None
+
+    first = second = head
+
+    for _ in range(k):
+        if not first:
+            return None #k is after the end of the array
+        else:
+            first = first.next
+
+
+    while first:
+        first = first.next
+        second = second.next
+
+    return second
