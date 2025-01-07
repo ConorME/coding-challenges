7d413b85b7e2c752bb9a27ddf447380e41626d2d
Conor <conor.esterly@gmail.com>
2025-01-07 16:49:02 -0500

    Additional SecretMessage changes.

--- Haskell/SecretMessage.hs
+++ Haskell/SecretMessage.hs
@@ -1,3 +1,8 @@
+{-# LANGUAGE UnicodeSyntax #-}
+module SecretMessage where
+
+import System.Random (randomRIO)
+
 data FourLetterAlphabet = L1 | L2 | L3 | L4 deriving (Show, Enum, Bounded)
 
 rotN :: (Bounded a, Enum a) => Int -> a -> a
@@ -15,3 +20,20 @@ rotChar charToEncrypt = rotN sizeOfAlphabet charToEncrypt
 
 rotString :: String -> String
 rotString stringToEncrypt = map rotChar stringToEncrypt
+
+-- Three Letter Alphabet
+data ThreeLetterAlphabet = Ψ | Ω | Σ deriving (Show, Enum, Bounded)
+
+randomThreeLetter :: IO ThreeLetterAlphabet
+randomThreeLetter = do
+    let min = fromEnum (minBound :: ThreeLetterAlphabet)
+    let max = fromEnum (maxBound :: ThreeLetterAlphabet)
+    randomNum <- randomRIO (min, max)
+    return (toEnum randomNum)
+
+randomThreeLetterList :: Int -> IO [ThreeLetterAlphabet]
+randomThreeLetterList n = sequence (replicate n randomThreeLetter)
+
+threeLetterEncoder :: [ThreeLetterAlphabet] -> [ThreeLetterAlphabet]
+threeLetterEncoder vals = map rot3 vals
+   where rot3 = rotN 3
