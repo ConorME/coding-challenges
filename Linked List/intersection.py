c17da74a60153320d842ffc45e9bf3469bab2e5f
Conor <conor.esterly@gmail.com>
2025-01-10 15:28:57 -0500

    Added Linked List 2.7: Intersection

---
+++ Linked List/intersection.py
@@ -0,0 +1,18 @@
+def intersection(l1: Node, l2: Node):
+    seen = set()
+    
+    # Add all nodes of l1 to the set
+    current = l1
+    while current:
+        seen.add(current)
+        current = current.next
+
+    # Check if any node in l2 exists in l1
+    current = l2
+    while current:
+        if current in seen:
+            return current 
+        current = current.next
+        
+    return None
+
