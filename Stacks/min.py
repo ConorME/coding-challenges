b42284c8ae7d442d9289201cb3c09169d3975f5f
Conor <conor.esterly@gmail.com>
2025-01-15 17:42:20 -0500

    Adjusted Min Stack implementation

--- Stacks/min.py
+++ Stacks/min.py
@@ -1,25 +1,22 @@
-from Stack import Stack, Node
+from Stack import Stack
 
 class MinStack(Stack):
     def __init__(self):
         super().__init__()
-        self.min_stack = Stack()  
-
-    def push(self, item):
-        super().push(item)
-        if self.min_stack.is_empty() or item <= self.min_stack.peek():
-            self.min_stack.push(item)
-
-    def pop(self):
-        if self.is_empty():
-            raise Exception("Empty Stack Exception: pop used on empty stack")
-        item = super().pop()
-        if item == self.min_stack.peek():
-            self.min_stack.pop()
-        return item
-
-    def get_min(self):
+        self.min_stack = Stack()
+    
+    def min(self):
         if self.min_stack.is_empty():
-            raise Exception("Stack is empty, no minimum value.")
+            return None
         return self.min_stack.peek()
 
+    def push(self, item):
+        if self.min() is None or item <= self.min():
+            self.min_stack.push(item)
+        super().push(item)
+
+    def pop(self):
+        item = super().pop()
+        if item == self.min():
+            self.min_stack.pop()
+        return item
